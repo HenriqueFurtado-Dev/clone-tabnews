ea73502006530a0d29eecd6cce1204a1a34af3ad
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2025-01-30 18:10:49 +0000

    Add  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,10 @@
-function status(request, response) {
-  response.status(200).json(
-    {
-      chave: "Alunos do curso.dev são gente boa",
-      outrachave: "Outro valor"
-    }
-  )
+import database from "../../../../infra/database.js"
+
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1;');
+  console.log(result.rows)
+  response.status(200).json({chave: "Alunos do curso.dev são gente boa"})
 }
 
-export default status;
+export default status; 
