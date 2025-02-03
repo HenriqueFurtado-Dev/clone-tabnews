12528077ee26262773beb6f4ade79cc6e14cfc89
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2025-02-03 18:37:19 +0000

    adds  endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,42 @@
+import migrationRunner from 'node-pg-migrate';
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient, 
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations", // Define table to save logs
+  }
+
+  if (request.method == "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method == "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions, // Spread = copia e cola as informações
+      dryRun: false,
+    });
+
+    await dbClient.end(); // Precisamos encerrar a conexão manualmente
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);  
+    }
+
+    return response.status(200).json(migratedMigrations);
+  }
+
+
+    
+    response.status(405).end();
+} 
